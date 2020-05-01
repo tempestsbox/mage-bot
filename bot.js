d038ce37e8c81b4f13ad4304b323ccf2df07c78d
Glitch (ttb-bot) <none>
2020-05-01 22:19:08 +0000

    Add suggest command

--- bot.js
+++ bot.js
@@ -20,6 +20,7 @@ const config = require("./config.json");
 const package = require("./package.json");
 
 var lastChainStoryMessage;
+var loaded = false;
 
 client.on("ready", () => {
   console.log(
@@ -27,18 +28,28 @@ client.on("ready", () => {
   );
   client.user.setActivity(config.activity);
 
+  if (!loaded)
+      client.channels
+          .fetch(config.bot_channel)
+          .then((channel) => channel.send("Loaded!"))
+          .catch(console.error);
+  loaded = true;
+
+  keepAlive();
+});
+
+function keepAlive() {
   const http = require("http");
   const express = require("express");
   const app = express();
   app.get("/", (request, response) => {
-    process.exit();
-    response.sendStatus(200);
+      process.exit();
   });
   app.listen(process.env.PORT);
   setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+      http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
   }, 280000);
-});
+}
 
 client.on("message", async message => {
   if (message.author.bot) {
@@ -62,24 +73,24 @@ client.on("message", async message => {
   if (!isCommand && config.postMessagesToLog)
     console.log(
       "[#" +
-        message.channel.name +
-        "] " +
-        message.author.tag +
-        ": " +
-        message.content
+      message.channel.name +
+      "] " +
+      message.author.tag +
+      ": " +
+      message.content
     );
 
   // if not command, return
   if (!message.content.startsWith(config.prefix)) return;
 
   if (!isCommand) return;
-  
+
   if (client.commands.get(command).required_permissions != null) {
     var vreturn = false;
 
     client.commands.get(command).required_permissions.forEach((item, index) => {
       if (!message.member.hasPermission(item)) {
-        if (!vreturn) message.channel.send('<@' + message.author.id + '>, you do not have the required permissions! `' + client.commands.get(command).required_permissions+ '`');
+        if (!vreturn) message.channel.send('<@' + message.author.id + '>, you do not have the required permissions! `' + client.commands.get(command).required_permissions + '`');
         vreturn = true;
       };
     });
@@ -89,21 +100,21 @@ client.on("message", async message => {
 
   try {
     console.log(
-        "---> " +
-        message.author.tag +
-        " ran command `" +
-        message.content +
-        "` in #" +
-        message.channel.name
+      "---> " +
+      message.author.tag +
+      " ran command `" +
+      message.content +
+      "` in #" +
+      message.channel.name
     );
     client.commands.get(command).execute(message, args);
   } catch (error) {
     console.error(error);
     message.reply(
       "there was an error trying to execute that command!" +
-        "\n`" +
-        error.message +
-        "`"
+      "\n`" +
+      error.message +
+      "`"
     );
   }
 });
--- commands/suggest.js
+++ commands/suggest.js
@@ -1,11 +1,36 @@
 /* eslint-disable no-unused-vars */
-const Discord = require('discord.js');
-const config = require('../config.json');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("../config.json");
 
 module.exports = {
-	aliases: ['suggest', 'sug', 's'],
-	description: 'Suggest a feature or improvement for TTB!',
-	execute(message, args) {
-		message.channel.send('Suggest ideas in <#576174992283140116> or at **https://reddit.com/' + config.reddit + '**!');
-	},
+    aliases: ["suggest", "sug", "s"],
+    description: "Suggest a feature or improvement for TTB!",
+    async execute(message, args) {
+        const feedback_prefix = '<@' + message.author.id + '>, ';
+
+        if (args[0] == null || args[0] == undefined || args[0] == '') {
+            message.channel.send(feedback_prefix + 'you did not enter a suggestion!');
+            return;
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setDescription(args.join(" "))
+            .setColor('0x9b9b9b')
+            .setTimestamp()
+            .setFooter('Suggestion', message.author.avatarURL())
+            .setAuthor(message.author.tag);
+
+        const channel = await client.channels.fetch(config.suggestions_channel);
+
+        try {
+            channel.send(embed).then(() => {
+                message.channel.send(feedback_prefix + 'your suggestion has been posted in <#' + channel + '>!');
+            });
+        } catch (error) {
+            message.channel.send('`' + error.message + '` - Please keep trying the command! It works eventually 😛');
+        }
+    },
 };
+
+client.login(process.env.TOKEN);
