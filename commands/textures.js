d80e5c4753f0a6f59e10e4bcac82a13b2e27d113
Daniel Orr <orrdaniel73@gmail.com>
2020-07-24 09:43:21 +0100

    Update textures.js

--- commands/textures.js
+++ commands/textures.js
@@ -3,6 +3,6 @@ module.exports = {
     aliases: ['textures'],
     description: 'A list of textures that need to be done',
     execute(message, args) {
-        message.channel.send('**Textures List**\n<https://www.guilded.gg/thetempestsbox/groups/7dL7rYoD/channels/735d93db-dc89-4de5-a660-5dce1328c483/list>');
+        message.channel.send('**Textures List**\n<https://trello.com/b/lsQB6Bue?menu=filter&filter=label:Graphics%20Required>');
     },
 };
